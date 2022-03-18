84e26a0aa8ad26b43548f8c0382d79472e366bff
Lani <lani.mortontran@movemedical.com>
2022-03-18 11:10:34 -0700

    Section 9: Completed Xylophone app

--- lib/main.dart
+++ lib/main.dart
@@ -4,22 +4,49 @@ import 'package:flutter/material.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  List<Color> keyColors = [
+    Colors.red,
+    Colors.orange,
+    Colors.yellow,
+    Colors.green,
+    Colors.green[900],
+    Colors.blue[800],
+    Colors.deepPurple,
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Center(
-            child: TextButton(
-              onPressed: () {
-                AudioCache audioPlayer = AudioCache();
-                audioPlayer.play('note1.wav');
-              },
-              child: Text('Click Me'),
-            ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: List.generate(keyColors.length, buildKey),
           ),
         ),
       ),
     );
+  }
+
+  Widget buildKey(int key) {
+    return Expanded(
+      child: TextButton(
+        style: TextButton.styleFrom(
+          backgroundColor: keyColors[key],
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.zero),
+          ),
+        ),
+        onPressed: () => playSound(key + 1),
+        child: null,
+      ),
+    );
+  }
+
+  void playSound(int keyNumber) {
+    AudioCache audioPlayer = AudioCache();
+    audioPlayer.play('note$keyNumber.wav');
   }
 }
